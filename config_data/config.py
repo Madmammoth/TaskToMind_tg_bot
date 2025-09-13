02499f408707953586577dba0cf60fd5d7882d47
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-09-13 08:26:14 +0300

    Added logging configuration

--- config_data/config.py
+++ config_data/config.py
@@ -7,8 +7,14 @@ class BotSettings(BaseModel):
     owner_id: SecretStr
 
 
+class LogSetting(BaseModel):
+    level: str
+    format: str
+
+
 class Config(BaseModel):
     bot_settings: BotSettings
+    log_settings: LogSetting
 
 
 def load_config(path: str | None = None) -> Config:
@@ -18,5 +24,9 @@ def load_config(path: str | None = None) -> Config:
         bot_settings=BotSettings(
             token=env("BOT_TOKEN"),
             owner_id=env("OWNER_ID")
+        ),
+        log_settings=LogSetting(
+            level=env("LOG_LEVEL"),
+            format=env("LOG_FORMAT")
         )
     )
