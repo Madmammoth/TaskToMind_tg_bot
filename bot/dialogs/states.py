72a36b48aef405f94fe776081ff65bb8ee93fae7
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-09-20 09:45:20 +0300

    feat: Add the start dialog, the settings dialog, rename the windows in the task addition dialog.

--- bot/dialogs/states.py
+++ bot/dialogs/states.py
@@ -1,6 +1,15 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
+class StartSG(StatesGroup):
+    start_window = State()
+
+
 class GetTaskDialogSG(StatesGroup):
-    menu_window = State()
-    lists_window = State()
+    tasks_window = State()
+    create_task_window = State()
+    cancel_window = State()
+
+
+class ChangeSettingsDialogSG(StatesGroup):
+    settings_window = State()
