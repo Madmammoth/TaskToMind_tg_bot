071a20d9891018d251755f87bdacb65c1602e720
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-09-20 14:14:04 +0300

    refactor: Transferring handlers for dialog from the basic handlers to a separate dialog directory

---
+++ bot/flows/add_task/handlers.py
@@ -0,0 +1,64 @@
+from aiogram.types import CallbackQuery
+from aiogram_dialog import DialogManager, StartMode
+from aiogram_dialog.widgets.kbd import Button
+
+from bot.flows.start.states import GetTaskDialogSG, StartSG
+from bot.handlers.basic import logger
+
+
+async def go_tasks(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Заглушка-переход в окно с задачами. Функция %s",
+        go_tasks.__name__
+    )
+    await callback.answer(
+        "Когда-нибудь тут будет переход к уже добавленным задачам"
+    )
+
+
+async def go_back_to_add_task(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    await dialog_manager.switch_to(state=GetTaskDialogSG.add_task_window)
+
+
+async def go_cancel_yes(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    message_id = dialog_manager.start_data["message_id"]
+    await callback.bot.send_message(
+        chat_id=callback.message.chat.id,
+        text="Добавление задачи было отменено",
+        reply_to_message_id=message_id,
+    )
+    await callback.message.delete()
+    await dialog_manager.start(state=StartSG.start_window,
+                               mode=StartMode.RESET_STACK)
+
+
+async def go_inbox(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    pass
+
+
+async def go_cancel(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Переход в окно отмены добавления задачи. Функция %s",
+        go_cancel.__name__
+    )
+    await dialog_manager.switch_to(state=GetTaskDialogSG.cancel_window)
---
+++ bot/flows/start/handlers.py
@@ -0,0 +1,76 @@
+from aiogram.types import Message, CallbackQuery
+from aiogram_dialog import DialogManager
+from aiogram_dialog.widgets.input import ManagedTextInput
+from aiogram_dialog.widgets.kbd import Button
+
+from bot.flows.start.states import GetTaskDialogSG, StartSG
+from bot.handlers.basic import logger
+
+
+async def add_task(
+        message: Message,
+        widget: ManagedTextInput,
+        dialog_manager: DialogManager,
+        html_text: str
+):
+    logger.debug(
+        "Запуск диалога добавления задачи. Функция %s",
+        add_task.__name__
+    )
+    await dialog_manager.start(
+        state=GetTaskDialogSG.add_task_window,
+        data={"message_id": message.message_id, "task": html_text})
+
+
+async def input_task(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Переход в окно запроса задачи. Функция %s",
+        input_task.__name__
+    )
+    await dialog_manager.switch_to(state=StartSG.input_task_window)
+
+
+async def go_settings(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Заглушка-переход в окно настроек. Функция %s",
+        go_settings.__name__
+    )
+    await callback.answer(
+        "Когда-нибудь тут будет переход в настройки бота"
+    )
+
+
+async def go_features(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Заглушка-переход в окно возможностей бота. Функция %s",
+        go_features.__name__
+    )
+    await callback.answer(
+        "Когда-нибудь тут будет переход к описанию возможностей бота"
+    )
+
+
+async def go_support(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Заглушка-переход в окно технической поддержки. Функция %s",
+        go_support.__name__
+    )
+    await callback.answer(
+        "Когда-нибудь тут будет соединение с технической поддержкой"
+    )
