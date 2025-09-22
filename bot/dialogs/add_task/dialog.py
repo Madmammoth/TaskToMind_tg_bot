46dcd5e13095e00659ebf2a652afbd4129e3619a
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-09-22 13:27:37 +0300

    feature: Add the display of default values to the task settings window, add the task saving to the database window, and process its button clicks

--- bot/dialogs/add_task/dialog.py
+++ bot/dialogs/add_task/dialog.py
@@ -4,15 +4,23 @@ from aiogram_dialog.widgets.text import Format, Const
 
 from bot.dialogs.states import GetTaskDialogSG
 from bot.dialogs.add_task.getters import get_task
-from bot.dialogs.add_task.handlers import go_pass, go_cancel_yes
+from bot.dialogs.add_task.handlers import go_pass, go_cancel_yes, go_save_yes
 
 add_task_dialog = Dialog(
     Window(
         Format("{task}"),
+        Format("\nСписок: {in_list}"),
+        Format("Приоритет: {priority}"),
+        Format("Срочность: {urgency}"),
         Column(
-            Button(
+            SwitchTo(
                 text=Const("Сохранить"),
                 id="save",
+                state=GetTaskDialogSG.save_window
+            ),
+            Button(
+                text=Const("Список"),
+                id="in_list",
                 on_click=go_pass
             ),
             Button(
@@ -40,11 +48,14 @@ add_task_dialog = Dialog(
     ),
     Window(
         Format("{task}"),
+        Format("\nСписок: {in_list}"),
+        Format("Приоритет: {priority}"),
+        Format("Срочность: {urgency}"),
         Column(
-            Button(
+            SwitchTo(
                 text=Const("Сохранить"),
                 id="save",
-                on_click=go_pass
+                state=GetTaskDialogSG.save_window
             ),
             Button(
                 text=Const("Срок завершения"),
@@ -62,33 +73,23 @@ add_task_dialog = Dialog(
                 on_click=go_pass
             ),
             Button(
-                text=Const("В список"),
-                id="to_list",
-                on_click=go_pass
-            ),
-            Button(
                 text=Const("Чек-лист"),
                 id="check_list",
                 on_click=go_pass
             ),
-        Column(
-            Button(
-                text=Const("Отложить"),
-                id="delay",
-                on_click=go_pass
-            ),
-            Button(
-                text=Const("Повтор"),
-                id="delay",
-                on_click=go_pass
-            ),
-            Button(
-                text=Const("Изменить список"),
-                id="change_list",
-                on_click=go_pass
+            Column(
+                Button(
+                    text=Const("Отложить"),
+                    id="delay",
+                    on_click=go_pass
+                ),
+                Button(
+                    text=Const("Повтор"),
+                    id="delay",
+                    on_click=go_pass
+                ),
             ),
         ),
-            ),
         Back(
             text=Const("Основные настройки"),
             id="back",
@@ -100,6 +101,35 @@ add_task_dialog = Dialog(
         ),
         getter=get_task,
         state=GetTaskDialogSG.add_task_window_2
+    ),
+    Window(
+        Const("Задача:\n"),
+        Format("{task}\n"),
+        Const("будет сохранена со следующими параметрами:"),
+        Format("\nСписок: {in_list}"),
+        Format("Приоритет: {priority}"),
+        Format("Срочность: {urgency}"),
+        Format("Срок завершения: {deadline}", when="deadline_show"),
+        Format("Продолжительность: {duration}", when="duration_show"),
+        Format("Напомнить: {remind}", when="remind_show"),
+        Format("Повтор: {repeat}", when="repeat_show"),
+        Format("Чек-лист: {checklist}", when="checklist_show"),
+        Format("\nТег: {tag}", when="tag_show"),
+        Const("\nВсё верно?"),
+        Row(
+            Button(
+                text=Const("Да"),
+                id="yes",
+                on_click=go_save_yes,
+            ),
+            SwitchTo(
+                text=Const("Нет"),
+                id="no",
+                state=GetTaskDialogSG.add_task_window
+            ),
+        ),
+        getter=get_task,
+        state=GetTaskDialogSG.save_window
     ),
     Window(
         Const("Вы уверены, что хотите отменить добавление задачи?"),
--- bot/dialogs/add_task/handlers.py
+++ bot/dialogs/add_task/handlers.py
@@ -5,6 +5,7 @@ from aiogram_dialog import DialogManager, StartMode
 from aiogram_dialog.widgets.kbd import Button
 
 from bot.dialogs.states import StartSG
+from database.db import fake_database
 
 logger = logging.getLogger(__name__)
 
@@ -23,11 +24,58 @@ async def go_pass(
     )
 
 
+async def go_save_yes(
+        callback: CallbackQuery,
+        widget: Button,
+        dialog_manager: DialogManager
+):
+    logger.debug(
+        "Сохранение задачи. Функция %s",
+        go_save_yes.__name__
+    )
+    user_id = callback.from_user.id
+    username = callback.from_user.username
+    first_name = callback.from_user.first_name
+    message_id = dialog_manager.start_data["message_id"]
+    data = {**dialog_manager.start_data, **dialog_manager.dialog_data}
+    fake_database[user_id][username] = username
+    fake_database[user_id][first_name] = first_name
+    fake_database[user_id]["lists"][data["in_list"]][message_id] = {
+        "task": data["task"],
+        "priority": data.get("priority"),
+        "urgency": data.get("urgency"),
+        "deadline": data.get("deadline"),
+        "deadline_show": data.get("deadline_show", False),
+        "duration": data.get("duration"),
+        "duration_show": data.get("duration_show", False),
+        "remind": data.get("remind"),
+        "remind_show": data.get("remind_show", False),
+        "repeat": data.get("repeat"),
+        "repeat_show": data.get("repeat_show", False),
+        "checklist": data.get("checklist"),
+        "checklist_show": data.get("checklist_show", False),
+        "tag": data.get("tag"),
+        "tag_show": data.get("tag_show", False),
+    }
+    await callback.bot.send_message(
+        chat_id=callback.message.chat.id,
+        text="Задача успешно добавлена!",
+        reply_to_message_id=message_id,
+    )
+    await callback.message.delete()
+    await dialog_manager.start(state=StartSG.start_window,
+                               mode=StartMode.RESET_STACK)
+
+
 async def go_cancel_yes(
         callback: CallbackQuery,
         widget: Button,
         dialog_manager: DialogManager
 ):
+    logger.debug(
+        "Отмена добавления задачи. Функция %s",
+        go_cancel_yes.__name__
+    )
     message_id = dialog_manager.start_data["message_id"]
     await callback.bot.send_message(
         chat_id=callback.message.chat.id,
