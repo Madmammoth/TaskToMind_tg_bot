d52bcf49ce5587aaab8e3f108d9a5a1ba68f8814
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-10-06 13:51:27 +0300

    fix: Add the creation of the engine and session maker when launching the bot, proper initialization of middleware for database connection

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,18 @@
 import asyncio
 import logging
+import os
+import sys
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram_dialog import setup_dialogs
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from bot.dialogs import dialogs
 from bot.handlers import routers
 from config_data.config import Config, load_config
-from database.middlewares import middlewares
+from database.middlewares import DbSessionMiddleware
 
 config: Config = load_config()
 
@@ -20,9 +23,18 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+if sys.platform.startswith("win") or os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 
 async def main():
     logger.info("Starting bot...")
+    dsn = config.db_settings.get_dsn()
+    engine = create_async_engine(
+        url=dsn,
+        echo=True,
+    )
+
     bot = Bot(
         token=config.bot_settings.token.get_secret_value(),
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
@@ -33,7 +45,8 @@ async def main():
     dp.include_routers(*dialogs)
     setup_dialogs(dp)
     logger.info("Including middlewares...")
-    dp.update.middleware(*middlewares)
+    Sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    dp.update.middleware(DbSessionMiddleware(Sessionmaker))
     try:
         await dp.start_polling(
             bot,
