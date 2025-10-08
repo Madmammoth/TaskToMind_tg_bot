dcf8953163731a0f4126593331fa3802eb3b1def
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-10-08 12:15:45 +0300

    fix: Set default values in separate functions, add temporary language labels

--- bot/handlers/task_messages.py
+++ bot/handlers/task_messages.py
@@ -1,26 +1,49 @@
 import logging
+from typing import Any
 
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram_dialog import DialogManager
 
 from bot.dialogs.states import GetTaskDialogSG
+from database.models import LevelEnum
+from locales.ru import PRIORITY_LABELS, URGENCY_LABELS
 
 logger = logging.getLogger(__name__)
 
 task_messages_router = Router()
 
 
+def split_title_and_description(text: str):
+    cleaned = text.strip()
+    if not cleaned:
+        return "Моя задача", None
+    lines = cleaned.split("\n", 1)
+    title = lines[0].strip()
+    description = lines[1].strip() if len(lines) > 1 else None
+    return title, description
+
+
+def make_default_task_data(message_id, task_text) -> dict[str, str | Any]:
+    task_title, task_description = split_title_and_description(task_text)
+    task_data = {
+        "message_id": message_id,
+        "task_title": task_title,
+        "task_description": task_description,
+        "list_name": "Входящие",
+        "priority": LevelEnum.MEDIUM,
+        "priority_label": PRIORITY_LABELS[LevelEnum.MEDIUM],
+        "urgency": LevelEnum.MEDIUM,
+        "urgency_label": URGENCY_LABELS[LevelEnum.MEDIUM],
+    }
+    return task_data
+
+
 @task_messages_router.message(F.text)
 async def get_task_handler(message: Message, dialog_manager: DialogManager):
     logger.debug("Апдейт попал в хэндлер %s", get_task_handler.__name__)
+    task_data = make_default_task_data(message.message_id, message.html_text)
     await dialog_manager.start(
         state=GetTaskDialogSG.add_task_window,
-        data={
-            "message_id": message.message_id,
-            "task": message.html_text,
-            "in_list": "Входящие",
-            "priority": "Низкий",
-            "urgency": "Низкая",
-        }
+        data=task_data
     )
