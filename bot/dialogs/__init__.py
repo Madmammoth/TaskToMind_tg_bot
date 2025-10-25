7f2e36b81b3ade86dc752ac61692d6d280c80bf2
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-10-25 13:03:11 +0300

    feature: Add a dialog for testing aiogram-dialogs features

--- bot/dialogs/__init__.py
+++ bot/dialogs/__init__.py
@@ -2,8 +2,10 @@ __all__ = ["dialogs"]
 
 from .add_task.dialog import add_task_dialog
 from .start.dialog import start_dialog
+from .testing.dialog import test_dialog
 
 dialogs = [
     start_dialog,
     add_task_dialog,
+    test_dialog,
 ]
--- bot/dialogs/start/dialog.py
+++ bot/dialogs/start/dialog.py
@@ -1,43 +1,61 @@
 from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.input import TextInput
-from aiogram_dialog.widgets.kbd import Row, Button
+from aiogram_dialog.widgets.kbd import Row, Button, Start
 from aiogram_dialog.widgets.text import Const
 
 from bot.dialogs.add_task.handlers import go_pass
-from bot.dialogs.states import StartSG
-from bot.dialogs.start.handlers import add_task, input_task, go_settings, go_features, go_support
+from bot.dialogs.start.handlers import (
+    add_task, input_task, go_settings, go_features, go_support,
+)
+from bot.dialogs.states import StartSG, ForTestsDialogSG
 
 start_dialog = Dialog(
     Window(
-        Const("Отправь мне планируемую задачу, "
-              "чтобы не забыть о ней!"),
-        Row(
-            Button(text=Const("Добавить задачу"),
-                   id="add_task",
-                   on_click=input_task),
-            Button(text=Const("Посмотреть задачи"),
-                   id="tasks",
-                   on_click=go_pass),
+        Const(
+            "Отправь мне планируемую задачу, чтобы не забыть о ней!",
         ),
         Row(
-            Button(text=Const("Настройки"),
-                   id="settings",
-                   on_click=go_settings),
-            Button(text=Const("Возможности бота"),
-                   id="features",
-                   on_click=go_features),
+            Button(
+                text=Const("Добавить задачу"),
+                id="add_task",
+                on_click=input_task,
+            ),
+            Button(
+                text=Const("Посмотреть задачи"),
+                id="tasks",
+                on_click=go_pass,
+            ),
         ),
-        Button(text=Const("Поддержка"),
-               id="support",
-               on_click=go_support),
-        state=StartSG.start_window
+        Row(
+            Button(
+                text=Const("Настройки"),
+                id="settings",
+                on_click=go_settings,
+            ),
+            Button(
+                text=Const("Возможности бота"),
+                id="features",
+                on_click=go_features,
+            ),
+        ),
+        Button(
+            text=Const("Поддержка"),
+            id="support",
+            on_click=go_support,
+        ),
+        Start(
+            text=Const("Тестирование"),
+            id="testing",
+            state=ForTestsDialogSG.test_window,
+        ),
+        state=StartSG.start_window,
     ),
     Window(
-        Const("✍️ Введите текст задачи"),
+        Const("✍️ Введи текст задачи"),
         TextInput(
             id="task_input",
-            on_success=add_task
+            on_success=add_task,
         ),
-        state=StartSG.input_task_window
+        state=StartSG.input_task_window,
     ),
 )
