e19a28eb86a3351fb88d4c775e501f7e42b9d82d
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-10-25 13:01:51 +0300

    feature: Add a selection list window, add a dialog for testing aiogram-dialogs features

--- bot/dialogs/states.py
+++ bot/dialogs/states.py
@@ -10,11 +10,18 @@ class GetTaskDialogSG(StatesGroup):
     tasks_window = State()
     add_task_window = State()
     add_task_window_2 = State()
+    select_list_window = State()
     task_priority_window = State()
     task_urgency_window = State()
-    save_window = State()
+    save_task_window = State()
     cancel_window = State()
 
 
 class ChangeSettingsDialogSG(StatesGroup):
     settings_window = State()
+
+
+class ForTestsDialogSG(StatesGroup):
+    test_window = State()
+    main = State()
+    result = State()
