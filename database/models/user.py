ee283336a26d114a8d3ed8a79df5852a843efc8f
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-16 12:12:30 +0300

    refactor: Move statistics to a separate module

--- database/models/user.py
+++ database/models/user.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta
-from typing import Annotated
 
 from sqlalchemy import (
     BigInteger, String, Enum, SmallInteger, DateTime, Interval, func,
-    ForeignKey, Integer, and_
+    and_
 )
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -11,14 +10,10 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .achievement import UserAchievement
 from .base import Base, make_timestamp_mixin
 from .enums import AccessRoleEnum, GenderEnum
-from .tracking import Reminder, ActivityLog
 from .tag import Tag, UserTag
 from .task import Task, TaskAccess
 from .task_list import TaskList, ListAccess
-
-stats_counter = Annotated[
-    int, mapped_column(Integer, default=0, nullable=False)
-]
+from .tracking import Reminder, ActivityLog
 
 
 class User(Base, make_timestamp_mixin()):
@@ -180,54 +175,3 @@ class User(Base, make_timestamp_mixin()):
 
     def __repr__(self) -> str:
         return f"<User id={self.telegram_id}, username={self.username!r}>"
-
-
-class UserStats(Base, make_timestamp_mixin()):
-    __tablename__ = "user_stats"
-
-    user_id: Mapped[int] = mapped_column(
-        BigInteger,
-        ForeignKey("users.telegram_id", ondelete="CASCADE"),
-        primary_key=True,
-    )
-    tasks_created: Mapped[stats_counter]
-    tasks_completed: Mapped[stats_counter]
-    tasks_postponed: Mapped[stats_counter]
-    tasks_canceled: Mapped[stats_counter]
-    tasks_shared: Mapped[stats_counter]
-    shared_tasks_completed: Mapped[stats_counter]
-    shared_tasks_postponed: Mapped[stats_counter]
-    shared_tasks_canceled: Mapped[stats_counter]
-    recurring_tasks_created: Mapped[stats_counter]
-    recurring_tasks_deleted: Mapped[stats_counter]
-    low_priority_tasks_created: Mapped[stats_counter]
-    medium_priority_tasks_created: Mapped[stats_counter]
-    high_priority_tasks_created: Mapped[stats_counter]
-    low_urgency_tasks_created: Mapped[stats_counter]
-    medium_urgency_tasks_created: Mapped[stats_counter]
-    high_urgency_tasks_created: Mapped[stats_counter]
-    low_priority_tasks_completed: Mapped[stats_counter]
-    medium_priority_tasks_completed: Mapped[stats_counter]
-    high_priority_tasks_completed: Mapped[stats_counter]
-    low_urgency_tasks_completed: Mapped[stats_counter]
-    medium_urgency_tasks_completed: Mapped[stats_counter]
-    high_urgency_tasks_completed: Mapped[stats_counter]
-    postpones_per_task: Mapped[stats_counter]
-    tasks_completed_before_deadline: Mapped[stats_counter]
-    tasks_completed_after_deadline: Mapped[stats_counter]
-    checked_tasks_created: Mapped[stats_counter]
-    checked_tasks_completed: Mapped[stats_counter]
-    checked_tasks_canceled: Mapped[stats_counter]
-    lists_created: Mapped[stats_counter]
-    lists_deleted: Mapped[stats_counter]
-    lists_shared: Mapped[stats_counter]
-    tags_created: Mapped[stats_counter]
-    tags_deleted: Mapped[stats_counter]
-    tasks_tagged: Mapped[stats_counter]
-    tags_assigned: Mapped[stats_counter]
-    tags_per_task: Mapped[stats_counter]
-    reminders_created: Mapped[stats_counter]
-    reminders_deleted: Mapped[stats_counter]
-    recurring_reminders_created: Mapped[stats_counter]
-    recurring_reminders_deleted: Mapped[stats_counter]
-    recurrence_rules_created: Mapped[stats_counter]
