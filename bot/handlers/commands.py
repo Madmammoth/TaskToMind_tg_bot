4cc88665f9aa8f4c979408d18c3b5e819b15c94d
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-18 17:30:25 +0300

    feature: Add a handler for the help command

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,12 +1,12 @@
 import logging
 
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode, ShowMode
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.dialogs.states import StartSG
+from bot.dialogs.states import StartSG, HelpSG
 from database.services.user import upsert_user_with_log
 
 logger = logging.getLogger(__name__)
@@ -42,5 +42,30 @@ async def cmd_start(
     await dialog_manager.start(
         state=StartSG.start_window,
         mode=StartMode.RESET_STACK,
+        show_mode=ShowMode.DELETE_AND_SEND,
+    )
+
+
+@commands_router.message(Command("help"))
+async def cmd_help(
+        message: Message,
+        dialog_manager: DialogManager,
+        session: AsyncSession
+):
+    logger.debug("Сообщение попало в хэндлер %s", cmd_start.__name__)
+    user_id = message.from_user.id
+    username = message.from_user.username
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    await upsert_user_with_log(
+        session,
+        user_id=user_id,
+        first_name=first_name,
+        last_name=last_name,
+        username=username,
+    )
+
+    await dialog_manager.start(
+        state=HelpSG.help_window,
         show_mode=ShowMode.DELETE_AND_SEND,
     )
