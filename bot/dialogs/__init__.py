41ef239bd4a0fa76ef8031d3d38defd00b6f8e31
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-18 17:34:47 +0300

    misc: Move the creation of a new list in a separate dialog, move the task input window to the task creation dialog, add a handler for the help command, add a dialog for processing user messages during other dialogs

--- bot/dialogs/__init__.py
+++ bot/dialogs/__init__.py
@@ -1,14 +1,18 @@
 __all__ = ["dialogs"]
 
-from .add_task.dialog import add_task_dialog
+from .create_list.dialog import create_list_dialog
+from .create_task.dialog import create_task_dialog
 from .lists_managment.dialog import lists_management_dialog
-from .start.dialog import start_dialog
+from .start.dialog import start_dialog, help_dialog, predict_dialog
 from .tasks_management.dialog import tasks_management_dialog
 from .testing.dialog import test_dialog
 
 dialogs = [
     start_dialog,
-    add_task_dialog,
+    help_dialog,
+    predict_dialog,
+    create_task_dialog,
+    create_list_dialog,
     test_dialog,
     lists_management_dialog,
     tasks_management_dialog,
