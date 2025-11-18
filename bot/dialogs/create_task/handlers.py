b992d5febc559544bc38de50aa0233cf75455592
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-18 18:07:44 +0300

    refactor: Rename the add_task directory to create_task, move the task input window to the task creation dialog

--- bot/dialogs/create_task/handlers.py
+++ bot/dialogs/create_task/handlers.py
@@ -1,29 +1,18 @@
 import logging
-from typing import cast
+from typing import cast, Any
 
-from aiogram.types import CallbackQuery
-from aiogram_dialog import DialogManager, StartMode, ShowMode, SubManager
+from aiogram.types import CallbackQuery, Message
+from aiogram_dialog import DialogManager, ShowMode, SubManager
+from aiogram_dialog.widgets.input import ManagedTextInput, MessageInput
 from aiogram_dialog.widgets.kbd import Button
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.dialogs.states import StartSG, GetTaskDialogSG
+from bot.dialogs.states import CreateTaskDialogSG
 from database.models import LevelEnum
 from database.services.task import add_task_with_stats_achievs_log
 from locales.ru import PRIORITY_LABELS, URGENCY_LABELS
 
 logger = logging.getLogger(__name__)
-
-
-async def add_task_dialog_start(start_data, dialog_manager: DialogManager):
-    logger.debug("Объединение стартового и основного словарей диалога")
-    logger.debug("Исходные словари:")
-    logger.debug("start_data: %s", start_data)
-    logger.debug("dialog_manager.dialog_data: %s", dialog_manager.dialog_data)
-
-    dialog_manager.dialog_data.update(start_data)
-
-    logger.debug("Полученный словарь:")
-    logger.debug("dialog_manager.dialog_data: %s", dialog_manager.dialog_data)
 
 
 async def go_priority(
@@ -101,9 +90,10 @@ async def go_save_yes(
         text="Задача успешно добавлена!",
         reply_to_message_id=message_id,
     )
-    await callback.message.delete()
-    await dialog_manager.start(state=StartSG.start_window,
-                               mode=StartMode.RESET_STACK)
+    await dialog_manager.done(
+        result={"create_task": "done"},
+        show_mode=ShowMode.DELETE_AND_SEND
+    )
 
 
 async def select_list(
@@ -128,7 +118,7 @@ async def select_list(
     logger.debug("Словарь dialog_data:")
     logger.debug(dialog_manager.dialog_data)
     await callback.answer(f"Выбран список: {list_title}")
-    await dialog_manager.switch_to(state=GetTaskDialogSG.add_task_window)
+    await dialog_manager.switch_to(state=CreateTaskDialogSG.add_task_window)
     logger.debug(
         "Установлен список задач id=%s, title=%s",
         list_id, list_title
@@ -144,14 +134,108 @@ async def go_cancel_yes(
         "Отмена добавления задачи. Функция %s",
         go_cancel_yes.__name__
     )
-    message_id = dialog_manager.start_data["message_id"]
+    message_id = dialog_manager.dialog_data["message_id"]
     await callback.bot.send_message(
         chat_id=callback.message.chat.id,
         text="Добавление задачи было отменено",
         reply_to_message_id=message_id,
     )
-    await dialog_manager.start(
-        state=StartSG.start_window,
-        mode=StartMode.RESET_STACK,
+    await dialog_manager.done(show_mode=ShowMode.DELETE_AND_SEND)
+
+
+def split_title_and_description(text: str):
+    cleaned = text.strip()
+    if not cleaned:
+        return "Моя задача", None
+    lines = cleaned.split("\n", 1)
+    title = lines[0].strip()
+    description = lines[1].strip() if len(lines) > 1 else None
+    return title, description
+
+
+def make_default_task_data(message_id, task_text) -> dict[str, str | Any]:
+    task_title, task_description = split_title_and_description(task_text)
+    task_data = {
+        "message_id": message_id,
+        "task_title": task_title,
+        "task_description": task_description,
+        "list_title": "Входящие",
+        "priority": LevelEnum.LOW,
+        "priority_label": PRIORITY_LABELS[LevelEnum.LOW],
+        "urgency": LevelEnum.LOW,
+        "urgency_label": URGENCY_LABELS[LevelEnum.LOW],
+        "show_lists_mode": "create_task",
+    }
+    return task_data
+
+
+def empty_text_check(text: str) -> str:
+    if text.strip():
+        return text
+    raise ValueError
+
+
+async def correct_text_task_input(
+        message: Message,
+        _widget: ManagedTextInput,
+        dialog_manager: DialogManager,
+        _text: str,
+):
+    logger.debug("Запуск диалога добавления задачи")
+    task_data = make_default_task_data(message.message_id, message.html_text)
+    dialog_manager.dialog_data.update(task_data)
+
+    await dialog_manager.switch_to(
+        state=CreateTaskDialogSG.add_task_window,
         show_mode=ShowMode.DELETE_AND_SEND,
     )
+
+
+async def empty_text_input(
+        message: Message,
+        _widget: ManagedTextInput,
+        dialog_manager: DialogManager,
+        _error: ValueError
+):
+    await message.answer("Тут же нет текста 🤔")
+    await dialog_manager.switch_to(
+        state=CreateTaskDialogSG.input_task_window,
+        show_mode=ShowMode.DELETE_AND_SEND,
+    )
+
+
+async def wrong_text_task_input(
+        message: Message,
+        _widget: MessageInput,
+        dialog_manager: DialogManager,
+):
+    logger.debug("Неправильный ввод текста задачи")
+    await message.answer("Пожалуйста, отправь именно текстовое сообщение!")
+    await dialog_manager.switch_to(
+        state=CreateTaskDialogSG.input_task_window,
+        show_mode=ShowMode.DELETE_AND_SEND,
+    )
+
+
+async def update_data(
+        _start_data,
+        result,
+        dialog_manager: DialogManager,
+):
+    logger.debug(
+        "Передача словаря result=%s из диалога создания списка "
+        "в диалог создания задачи",
+        result,
+    )
+    if result:
+        dialog_manager.dialog_data.update(result),
+        await dialog_manager.switch_to(
+            state=CreateTaskDialogSG.add_task_window,
+            show_mode=ShowMode.DELETE_AND_SEND,
+        )
+        logger.debug(
+            "Переход в диалог создания задачи "
+            "в окно настройки создаваемой задачи"
+        )
+    else:
+        logger.debug("Переход в диалог создания задачи в окно выбора списка")
