cabe44f10fa4895a2eaa2d86e7048c2c501dad53
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-18 18:11:43 +0300

    refactor: Move the creation of a new list in a separate dialog, move the task input window to the task creation dialog, add a handler for the help command, add a dialog for processing user messages during other dialogs

--- bot/dialogs/states.py
+++ bot/dialogs/states.py
@@ -3,10 +3,18 @@ from aiogram.fsm.state import StatesGroup, State
 
 class StartSG(StatesGroup):
     start_window = State()
+
+
+class HelpSG(StatesGroup):
+    help_window = State()
+
+
+class PredictSG(StatesGroup):
+    predict_window = State()
+
+
+class CreateTaskDialogSG(StatesGroup):
     input_task_window = State()
-
-
-class GetTaskDialogSG(StatesGroup):
     tasks_window = State()
     add_task_window = State()
     add_task_window_2 = State()
@@ -25,18 +33,21 @@ class TaskManagementDialogSG(StatesGroup):
     main_tasks_window = State()
 
 
-class TaskListsDialogSG(StatesGroup):
-    main_lists_window = State()
+class CreateListDialogSG(StatesGroup):
     input_list_title_window = State()
     add_list_window = State()
-    rename_new_list_window = State()
     in_list_window = State()
+    cancel_window = State()
+
+
+class TaskListsDialogSG(StatesGroup):
+    main_lists_window = State()
+    rename_new_list_window = State()
     save_list_window = State()
     list_with_tasks = State()
     rename_list_window = State()
     move_list_window = State()
     delete_list_window = State()
-    cancel_window = State()
     change_view_window = State()
 
 
