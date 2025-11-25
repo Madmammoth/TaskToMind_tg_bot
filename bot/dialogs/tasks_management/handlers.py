d96885db735da048c242686070cf05217303b800
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-25 11:56:38 +0300

    misc: Change data for start data, replace loggers

--- bot/dialogs/tasks_management/handlers.py
+++ bot/dialogs/tasks_management/handlers.py
@@ -15,23 +15,18 @@ async def go_selected_task(
         _widget: Button,
         dialog_manager: DialogManager,
 ):
-    logger.debug("Переход к задаче...")
+    logger.debug("Переход к задаче пользователя...")
+    user_id = callback.from_user.id
+    logger.debug("...id=%d", user_id)
     sub_manager = cast(SubManager, dialog_manager)
     dialog_manager = sub_manager.manager
-    task_id = str(sub_manager.item_id)
-    tasks = dialog_manager.dialog_data.get("tasks")
-    task_title = tasks[task_id]
-    dialog_manager.dialog_data.update({
-        "task_id": task_id,
-        "task_title": task_title,
-    })
-    data = {}
-    await callback.answer(f"Выбрана задача: {task_title}")
+    task_id = int(sub_manager.item_id)
+    data = {"task_id": task_id}
+    logger.debug(
+        "Пользователем id=%d выбрана задача id=%s, data=%s",
+        user_id, task_id, data
+    )
     await dialog_manager.start(
         state=TaskActionsDialogSG.main_task_window,
         data=data,
-    )
-    logger.debug(
-        "Выбрана задача id=%s, title=%s",
-        task_id, task_title
     )
