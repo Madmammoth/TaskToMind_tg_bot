2694d383a01c912bdcf69bd9db9aa8151bf1f2d9
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2025-11-25 11:57:35 +0300

    feature: Add task status labels and access labels

--- locales/ru.py
+++ locales/ru.py
@@ -1,4 +1,4 @@
-from database.models import LevelEnum
+from database.models import LevelEnum, TaskStatusEnum, AccessRoleEnum
 
 PRIORITY_LABELS = {
     LevelEnum.HIGH: "Высокий",
@@ -9,4 +9,15 @@ URGENCY_LABELS = {
     LevelEnum.HIGH: "Высокая",
     LevelEnum.MEDIUM: "Средняя",
     LevelEnum.LOW: "Низкая",
+}
+TASK_STATUS_LABELS = {
+    TaskStatusEnum.NEW: "Новая",
+    TaskStatusEnum.IN_PROGRESS: "В работе",
+    TaskStatusEnum.DONE: "Выполнена",
+    TaskStatusEnum.CANCELED: "Отменена",
+}
+ACCESS_LABELS = {
+    AccessRoleEnum.OWNER: "Владелец",
+    AccessRoleEnum.EDITOR: "Редактор",
+    AccessRoleEnum.VIEWER: "Наблюдатель",
 }
