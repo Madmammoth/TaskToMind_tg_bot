1e7634b79204a68ef678a47adae854440398bb1e
Marat Akhmetzyanov <akhmetzyanovmm@gmail.com>
2026-01-26 16:29:47 +0300

    refactor: Change the task priority test based on serialization

--- tests/bot/dialogs/create_task/test_go_priority.py
+++ tests/bot/dialogs/create_task/test_go_priority.py
@@ -10,5 +10,5 @@ async def test_priority_change(fake_dialog_manager, mock_callback):
 
     await go_priority(mock_callback, btn, fake_dialog_manager)  # noqa
 
-    assert fake_dialog_manager.dialog_data["priority"].value == "high"
+    assert fake_dialog_manager.dialog_data["priority"] == {'__enum__': 'LevelEnum.HIGH'}
     assert fake_dialog_manager.dialog_data["priority_label"] == "Высокий"
